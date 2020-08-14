b57fc35e7a0ea841c6aa77b329dd3692035e78ff
Garrett Brown <gman9mm@live.com>
2020-08-14 14:15:02 -0700

    Add proper identifiers in order for answers to show properly.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,16 @@
 $(document).ready(function () {
+  $(".answer-wrapper").hide();
+
   $("a.btn").click(function () {
     $("#language-quiz").show();
   });
 
   $("button.btn").click(function (event) {
     event.preventDefault();
-    const answer = $("#secret_question").val();
-    console.log("thisismyanswer", answer)
+    const answer = parseInt($("#secret_question").val());
     if (answer === 1) {
-      $(".answer-wrapper#answer-1").show();
+      console.log("answer");
+      $("#answer-1").show();
     }
     else if (answer === 2) {
       $(".answer-wrapper#answer-2").show();

417f0ae5d478f38c80803e1b21615089a6c7959b
Garrett Brown <gman9mm@live.com>
2020-08-14 13:50:25 -0700

    Add element detection, continuing troubleshooting.

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,27 @@
 $(document).ready(function () {
-  $(".btn").click(function () {
+  $("a.btn").click(function () {
     $("#language-quiz").show();
   });
 
-  $("language-quiz").submit(function (event) {
-    const answer = ($("secret_question").val());
-
-    let result;
-    if (value === 1) {
-      $("#answer-1").show();
+  $("button.btn").click(function (event) {
+    event.preventDefault();
+    const answer = $("#secret_question").val();
+    console.log("thisismyanswer", answer)
+    if (answer === 1) {
+      $(".answer-wrapper#answer-1").show();
     }
-    else if (value === 2) {
-      $("#answer-2").show();
+    else if (answer === 2) {
+      $(".answer-wrapper#answer-2").show();
     }
-    else if (value === 3) {
+    else if (answer === 3) {
       $("#answer-3").show();
     }
-    else if (value === 4) {
+    else if (answer === 4) {
       $("#answer-4").show();
     }
-    else if (value === 5) {
+    else if (answer === 5) {
       $("#answer-5").show();
     }
-    event.preventDefault();
+
   });
 });
